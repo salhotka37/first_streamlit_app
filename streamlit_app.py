b9f57a3d45796b916b881ff338748cbcffad1c91
salhotka37 <114019808+salhotka37@users.noreply.github.com>
2022-09-20 13:56:46 -0500

    Update streamlit_app.py
    Added bfast menu with emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 st.title('My Parents New Healthy Diner')
 st.header('Breakfast Menu')
-st.text('Omega 3 and Blueberry Oatmeal')
-st.text('Kale, Spinach and Rocket Smoothie')
-st.text('Hard-Boiled, Free-Range Egg')
+st.text('🥣 Omega 3 and Blueberry Oatmeal')
+st.text('🥗 Kale, Spinach and Rocket Smoothie')
+st.text('🐔 Hard-Boiled, Free-Range Egg')
+st.text('🥑🍞 Avocado Toast')
 
