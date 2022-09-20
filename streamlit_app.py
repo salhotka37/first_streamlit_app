23493b7f01b700d942ad024ca26911a6d271d90a
salhotka37 <114019808+salhotka37@users.noreply.github.com>
2022-09-20 14:27:42 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,9 +12,8 @@ st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-st.dataframe(my_fruit_list)
 
-fruits_selected = st.multiselect("Pick Some Fruits:", list(my_fruit_list.index),['Bananas', 'Strawberries'])
+fruits_selected = st.multiselect("Pick Some Fruits:", list(my_fruit_list.index), ['Bananas', 'Strawberries'])
 st.dataframe(fruits_selected)
 
 
